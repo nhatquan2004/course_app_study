74ae3bc45726d9c98cb49c1aee8f1c95b40ac1a2
nhatquan2004 <quanninja12@gmail.com>
2026-07-06 21:04:12 +0700

    chore: hehe2

--- frontend/src/features/courses/courseActions.ts
+++ frontend/src/features/courses/courseActions.ts
@@ -1,23 +1,27 @@
-'use server';
-import { createCourse, deleteCourse, updateCourse } from '@/services/courseService';
-import { CreateCoursePayload, UpdateCoursePayload } from './types';
+"use server";
+import {
+  createCourse,
+  deleteCourse,
+  updateCourse,
+} from "@/services/courseService";
+import { CreateCoursePayload, UpdateCoursePayload } from "./types";
 
 export async function createCourseAction(courseData: CreateCoursePayload) {
-	return await createCourse(courseData);
+  return await createCourse(courseData);
 }
 
-export async function updateCourseAction(courseId, formData: FormData) {
-	const courseUpdateData = {
-		name: formData.get('name'),
-		description: formData.get('description'),
-		coverImage: formData.get('coverImage'),
-		price: Number(formData.get('price')),
-		categoryId: Number(formData.get('categoryId')),
-	};
+export async function updateCourseAction(courseId: any, formData: FormData) {
+  const courseUpdateData = {
+    name: formData.get("name"),
+    description: formData.get("description"),
+    coverImage: formData.get("coverImage"),
+    price: Number(formData.get("price")),
+    categoryId: Number(formData.get("categoryId")),
+  };
 
-	return await updateCourse(courseId, courseUpdateData);
+  return await updateCourse(courseId, courseUpdateData);
 }
 
 export async function deleteCourseAction(courseId: string) {
-	return await deleteCourse(courseId);
+  return await deleteCourse(courseId);
 }
--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,31 +1,31 @@
-import { API_BASE_URL } from '@/config/api';
+import { API_BASE_URL } from "@/config/api";
 
-import axios from 'axios';
+import axios from "axios";
 
 type RegisterPayload = {
-	fullName: string;
-	username: string;
-	email: string;
-	password: string;
+  fullName: string;
+  username: string;
+  email: string;
+  password: string;
 };
 
 type LoginPayload = {
-	username: string;
-	password: string;
+  username: string;
+  password: string;
 };
 
 const instance = axios.create();
 
 export async function registerUser(userData: RegisterPayload) {
-	const res = await instance.post(`${API_BASE_URL}/user/create`, userData);
-	return res.data;
+  const res = await instance.post(`${API_BASE_URL}/user/create`, userData);
+  return res.data;
 }
 
-export async function loginUser(userData: LoginPayload) {
-	try {
-		const res = await instance.post(`${API_BASE_URL}/auth/login`, userData);
-		return res;
-	} catch (err) {
-		console.log(err);
-	}
+export async function loginUser(userData: any) {
+  try {
+    const res = await instance.post(`${API_BASE_URL}/auth/login`, userData);
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
 }
