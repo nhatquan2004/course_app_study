a04e069434c522872dd1e72d9437821c506b4478
Trong Phuc <trongphuc.workplace@gmail.com>
2026-07-19 10:41:48 +0700

    chore: commit

--- frontend/src/modules/user/components/UserList.tsx
+++ frontend/src/modules/user/components/UserList.tsx
@@ -72,6 +72,7 @@ export default function UserList({ users }: UserListProps) {
 				</div>
 			</div>
 
+			{/* {isAddModalOpen && <AddUserForm onClose={() => setIsAddModalOpen(false)} />} */}
 		</div>
 	);
 }
